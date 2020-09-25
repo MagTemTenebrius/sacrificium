7516307497971ebd1b109c6058e9d8373e60d92e
Aleksandr First <MagTemTenebrius@gmail.com>
2020-09-25 23:45:11 +0500

    update mapping, add biome def reg

--- src/main/java/ru/sacrificium/core/ObjectRegistration.java
+++ src/main/java/ru/sacrificium/core/ObjectRegistration.java
@@ -2,7 +2,10 @@ package ru.sacrificium.core;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeManager;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -11,13 +14,24 @@ import ru.sacrificium.util.Constants;
 public class ObjectRegistration {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Constants.MODID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Constants.MODID);
+    public static final DeferredRegister<Biome> BIOMES = DeferredRegister.create(ForgeRegistries.BIOMES, Constants.MODID);
+
+//    public static final RegistryObject<Biome> TEST_BIOME = BIOMES.register("test_biome", TestBiome::new);
 
     public static void register() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         BLOCKS.register(modEventBus);
         ITEMS.register(modEventBus);
+        BIOMES.register(modEventBus);
+
 
         SacrificiumItems.itemRegister();
         SacrificiumBlocks.blockRegistry();
+    }
+
+    public static void setupBiomes() {
+//        BiomeManager.addBiome(BiomeManager.BiomeType.COOL, new BiomeManager.BiomeEntry(TEST_BIOME.get(), 10));
+//
+//        BiomeDictionary.addTypes(TEST_BIOME.get(), Type.PLAINS, Type.OVERWORLD);
     }
 }
--- src/main/java/ru/sacrificium/core/SacrificiumEntities.java
+++ src/main/java/ru/sacrificium/core/SacrificiumEntities.java
@@ -15,7 +15,7 @@ public class SacrificiumEntities {
     // Entity Types
         public static final RegistryObject<EntityType<ShieldEntity>> SHIELD_ENTITY = ENTITY_TYPES.register("shield_type",
             () -> EntityType.Builder.<ShieldEntity>create(ShieldEntity::new, EntityClassification.MISC)
-                    .size(0.25F, 0.25F).func_233606_a_(4).func_233608_b_(10)
+                    .size(0.25F, 0.25F).func_233608_b_(10)
                     .build(new ResourceLocation(Constants.MODID, "shield_type").toString()));
 
 }
